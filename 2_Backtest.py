215b6afe63ced77cfa3c1e957f33f093a9a40bbe
krivylko-ctrl <krivylko@gmail.com>
2025-08-16 16:29:10 +0200

    Add files via upload

---
+++ 2_Backtest.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Backtest", page_icon="📈", layout="wide")
+st.title("Backtest")
+st.info("Временная заглушка. Страница отрисовывается корректно.")
---
+++ 3_Settings.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Settings", page_icon="⚙️", layout="wide")
+st.title("Settings")
+st.info("Временная заглушка. Настройки будут подключены позже.")
