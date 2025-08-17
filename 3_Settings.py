a3e159834dd81f35ec4e96868ce6ba52c50569f9
krivylko-ctrl <krivylko@gmail.com>
2025-08-17 12:25:06 +0200

    Delete 3_Settings.py

--- 3_Settings.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="Settings", page_icon="⚙️", layout="wide")
-st.title("Settings")
-st.info("Временная заглушка. Настройки будут подключены позже.")
